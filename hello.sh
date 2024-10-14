2b5a9d206e32d262cf6dcee8a3e53f0d5e68fd98
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-13 21:31:48 -0500

    add func

--- hello.sh
+++ hello.sh
@@ -3,6 +3,6 @@
 # This script is used to build the project and run the tests.
 # It assumes that the project is located in the parent directory of the current directory.
 
-hello(){
+function hello(){
     return "Hello, world!"
 }
