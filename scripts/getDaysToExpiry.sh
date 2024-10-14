1538e3c896ec9fe1c11efc4f3d3c093058fc68d1
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-14 07:26:27 -0500

    days

--- scripts/getDaysToExpiry.sh
+++ scripts/getDaysToExpiry.sh
@@ -3,7 +3,7 @@
 set -eu
 
 current_timestamp=$(date +%s)
-#echo "current_timestamp -> $current_timestamp"
+echo "current_timestamp -> $current_timestamp"
 
 #get expiration timestamp from reg-time.json
 
@@ -11,12 +11,11 @@ jq . reg-time.json
 
 future_date="$(jq -r '.expiration_date[:19]' reg-time.json)"
 future_timestamp=$(date -d "$future_date" +%s)
-#echo "future_timestamp -> $future_timestamp"
+echo "future_timestamp -> $future_timestamp"
 
 time_diff=$(($future_timestamp - $current_timestamp))
-#echo "time_diff -> $time_diff"
+echo "time_diff -> $time_diff"
 
 days=$(($time_diff / 86400))
-#echo "script days -> $days"
+echo "script days -> $days"
 
-echo "$days"
