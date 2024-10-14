e7e38ab404a7bf6e76fedc7d8b52e47e511090c1
nk1752 <71557302+nk1752@users.noreply.github.com>
2024-10-13 21:47:26 -0500

    vbnvbn

--- hello.sh
+++ hello.sh
@@ -3,6 +3,7 @@
 # This script is used to build the project and run the tests.
 # It assumes that the project is located in the parent directory of the current directory.
 
-function hello(){
-    echo "Hello, World!"
+hello(){
+    local name=$1
+    echo "Hello $name!"
 }
